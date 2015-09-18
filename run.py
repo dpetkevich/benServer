ad5aaa95714165f668019814aebc3dcdd236674d
Daniel Petkevich <danielpetkevich@Daniels-MacBook-Pro.local>
2015-09-18 15:53:37 -0700

    think i got it

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect
 import twilio.twiml
 from pb_py import main as api
+from bs4 import BeautifulSoup
 
 host = 'aiaas.pandorabots.com'
 user_key = '8704f84cef67d2c4c1c487ce9aab7da2'
@@ -15,16 +16,18 @@ def bot_talk():
     request_message = request.values.get('Body',  'Hi')
     bot_response = api.talk(user_key, app_id, host, botname, request_message)["response"]
     '''parse response'''
+    soup = BeautifulSoup(bot_response, "lxml")
     partition = bot_response.partition('<img')
     text_portion = partition[0]
-    image_portion = partition[1] + partition[2]
+    image_portion = soup.img.extract()['src']
     
     '''text response'''
     resp = twilio.twiml.Response()
     resp.message(msg=text_portion)
 
     '''image response'''
-    # resp.message().media(image_portion)
+    print(image_portion)
+    resp.message().media(image_portion)
     print(resp)
     return str(resp)
 
