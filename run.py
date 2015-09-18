bd36f2c8c2276f6d135b480439dd7459270b1526
Daniel Petkevich <danielpetkevich@Daniels-MacBook-Pro.local>
2015-09-18 13:59:22 -0700

    hello

--- run.py
+++ run.py
@@ -14,11 +14,21 @@ def bot_talk():
     """Respond to incoming texts with a text from your bot"""
     request_message = request.values.get('Body',  'Hi')
     bot_response = api.talk(user_key, app_id, host, botname, request_message)["response"]
-    resp = twilio.twiml.Response()
-    print(bot_response)
-    resp.message(msg=bot_response).media('https://i.chzbgr.com/full/8563718656/h636C9F57/')
-    print (str(resp))
-    return str(resp)
+    
+    '''text response'''
+    text_resp = twilio.twiml.Response()
+    text_resp.message(msg=bot_response)
+
+    '''image response'''
+    image_resp = twilio.twiml.Response()
+    image_resp.message().media('https://i.chzbgr.com/full/8563718656/h636C9F57/')
+
+    print str(text_resp) + str(image_resp)
+
+
+
+
+    return str(text_resp) + str(image_resp)
 
 if __name__ == "__main__":
     app.run(debug=True)
