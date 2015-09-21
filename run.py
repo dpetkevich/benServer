711c2652af2c271874a625bed55b6c57a45183d7
Daniel Petkevich <daniel.petkevich@gmail.com>
2015-09-21 15:45:41 -0700

    hi

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, make_response
 import twilio.twiml
 from pb_py import main as api
 from bs4 import BeautifulSoup
+from datetime import datetime,timedelta
 
 host = 'aiaas.pandorabots.com'
 user_key = '8704f84cef67d2c4c1c487ce9aab7da2'
@@ -15,7 +16,13 @@ def bot_talk():
     print(request.values)
     """Respond to incoming texts with a text from your bot"""
     request_message = request.values.get('Body','Hi')
-    full_bot_response = api.talk(user_key, app_id, host, botname, request_message, session_id=True, trace=True)
+
+    if request.cookies.get('session_id') != None:
+        session_id = int(request.cookies.get('session_id'))
+        full_bot_response = api.talk(user_key, app_id, host, botname, request_message, session_id, session_id=True, trace=True)
+    else:
+        full_bot_response = api.talk(user_key, app_id, host, botname, request_message, session_id=True, trace=True)
+
     '''parse response'''
     bot_response = full_bot_response["response"]
     session_response = full_bot_response["sessionid"]
@@ -29,8 +36,16 @@ def bot_talk():
     text_portion = partition[0]
     #image_portion = soup.img.extract()['src']
 
-    # construct response
+    # construct twiml response
     resp = twilio.twiml.Response()
+
+    #construction main respone
+    main_resp=make_response(str(resp))
+
+    #set cookie
+    expires=datetime.utcnow() + timedelta(hours=4)
+    main_resp.set_cookie('session_id',value=str(session_response),expires=expires.strftime('%a, %d %b %Y %H:%M:%S GMT'))
+
 
     '''text response'''
     #broken up responses
@@ -44,7 +59,7 @@ def bot_talk():
     '''image response'''
     # resp.message().media(image_portion)
     print(resp)
-    return str(resp)
+    return main_resp
 
 if __name__ == "__main__":
     app.run(debug=True)
