7030a62e2987a743c699ae14b3671b3ae62b031b
Daniel Petkevich <daniel.petkevich@gmail.com>
2015-09-22 10:50:58 -0700

    hi

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ import twilio.twiml
 from pb_py import main as api
 from bs4 import BeautifulSoup
 from datetime import datetime,timedelta
+import requests
+
 
 host = 'aiaas.pandorabots.com'
 user_key = '8704f84cef67d2c4c1c487ce9aab7da2'
@@ -15,32 +17,48 @@ app.secret_key="\xae\xfb\x10\xaa\x06l\x91\xaeg\xb3z\xa9j\x92\xcc\x08)\xa2\x1e\x9
 @app.route("/", methods=['GET','POST'])
 def bot_talk():
     print(request.values)
+    print('session')
     print(session.get('session_id'))
+    print('client')
+    print(session.get('client_name'))
     """Respond to incoming texts with a text from your bot"""
 
     request_message = request.values.get('Body','Hi')
-   
 
-    if session.get('session_id') != None:
-        session_id = session.get('session_id')
-        client_name = session_id
+    # if session.get('session_id') != None:
+    #     session_id = session.get('session_id')
+    #     client_name = session.get('client_name')
+    #     query = "https://aiaas.pandorabots.com/atalk/" + str(app_id) + "/" + str(botname) + "?user_key=" + str(user_key) + "&input=" + str(request_message) + '&client_name=' + str(client_name) + '&sessionid=' + str(session_id)
         
-        full_bot_response = api.talk(user_key, app_id, host, botname, request_message, session_id, client_name, trace=True)
+
+    # else:
+    #     query = "https://aiaas.pandorabots.com/atalk/" + str(app_id) + "/" + str(botname) + "?user_key=" + str(user_key) + "&input=" + str(request_message)
+
+    # r=requests.post(query)
+    # print(query)
+    # print(r.text)
+   
+    full_bot_response = api.talk(user_key, app_id, host, botname, request_message, trace=True)
 
 
-    else:
-        full_bot_response = api.talk(user_key, app_id, host, botname, request_message, trace=True)
-        session_id = full_bot_response["sessionid"]
+    # if session.get('session_id') != None:
+    #     # session_id = session.get('session_id')
+    #     # client_name = session_id
+        
+    #     full_bot_response = api.talk(user_key, app_id, host, botname, request_message, session_id, client_name, trace=True)
 
-    print("sessionid")
-    print(session_id)
-    print("client_name")
-    print(client_name)
+
+    # else:
+    #     full_bot_response = api.talk(user_key, app_id, host, botname, request_message, trace=True)
+    #     session_id = full_bot_response["sessionid"]
+
+
+
+
 
     '''parse response'''
     bot_response = full_bot_response["response"]
-    print(bot_response)
-    print(full_bot_response)
+ 
 
 
     soup = BeautifulSoup(bot_response, "lxml")
@@ -52,7 +70,7 @@ def bot_talk():
     resp = twilio.twiml.Response()
 
    
-    session['session_id'] = session_id
+    session.clear()
 
     '''text response'''
     #broken up responses
