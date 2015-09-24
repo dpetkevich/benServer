eb645fde7092c9766c7d6ba27bb3637c7b2b93b8
Daniel Petkevich <daniel.petkevich@gmail.com>
2015-09-24 10:52:22 -0700

    ready for twilio test

--- run.py
+++ run.py
@@ -4,9 +4,10 @@ from pb_py import main as api
 from bs4 import BeautifulSoup
 from datetime import datetime,timedelta
 import requests
+from zenpy import Zenpy
+import json
 # import mandrill
 # mandrill_client = mandrill.Mandrill('6SZYKk3ttHnLHjt9Z0D3xQ')
-
 
 host = 'aiaas.pandorabots.com'
 user_key = '8704f84cef67d2c4c1c487ce9aab7da2'
@@ -15,24 +16,85 @@ botname = 'benjamin'
 app = Flask(__name__)
 app.secret_key="\xae\xfb\x10\xaa\x06l\x91\xaeg\xb3z\xa9j\x92\xcc\x08)\xa2\x1e\x9aw9\xf8%"
 
+zenpy = Zenpy('textbenjamin', 'daniel@textbenjamin.com', 'SoC5ZLSVqnJHAUd2ABJj7AnEsqMeZqKHurrHV5Ij')
+
+user = 'daniel@textbenjamin.com'
+pwd = 'boris5423'
+
 
 @app.route("/", methods=['GET','POST'])
 def bot_talk():
     
     """Respond to incoming texts with a text from your bot"""
-    print("request form is")
-    print(request.form)
-    print("request args are")
-    print(request.args)
-    print("request json are")
-    print(request.json)
+   
 
-    front_response=requests.post("https://webhook.frontapp.com/sms/2fe99bece8c8c8dbe2eda50aa9ed3e4d772905b9bb8c74de423e9f3c3f2733c4", data = request.form)
+    # find user
+    # if users exists, find ticket, and add comment to ticket
+    # else, create new ticket for user and user implicitly
+    request_message = request.values.get('Body','Hi')
+    phone = request.values.get('From','+11111111111')
 
-    print(front_response)
+    # lookup whether this user is in zendesk
+    possible_existing_user = zenpy.search(type='user', phone=phone)
+
+    if possible_existing_user:
+
+        # lookup the ticket belonging to this user
+        relevant_ticket = zenpy.search(type='ticket', requester_id = possible_existing_user.next().id).next()
+
+        ## prepare update ticket request
+        # set url
+        url = 'https://textbenjamin.zendesk.com/api/v2/tickets/' + str(relevant_ticket.id) + '.json'
+
+        #set data 
+        data = {
+                    "ticket": {
+                        "comment":{ 
+                            "body": request_message
+                        }
+                    }
+                }
+
+        # set headers
+        headers = {'content-type': 'application/json'}
+
+        # make request
+        updated_ticket_response = requests.put(url, data = json.dumps(data), auth=(user, pwd), headers=headers)
+  
+
+    else:
+
+        ## prepare request to create ticket for this user
+        #set url
+        url = 'https://textbenjamin.zendesk.com/api/v2/tickets.json'
+
+        #set data 
+        data = {
+                    "ticket": {
+                        "requester": {
+                            "name" : "unnamed",
+                            "phone": phone
+                            },
+                        "comment":{ 
+                            "body": request_message
+                        },
+                        "subject": 'Request'
+                    }
+                }
+
+        # set headers
+        headers = {'content-type': 'application/json'}
+
+        # make request to create ticket
+        createTicketResponse = requests.post(url, data=json.dumps(data), auth=(user, pwd), headers=headers)
+
+        
+        
+
+
 
     #gets request body, default body to hi if it is emptpy
-    request_message = request.values.get('Body','Hi')
+    
 
     # calls the atalk endpoint with session_id and client_name is they exists in the session, otherwise without them
     if session.get('session_id') != None or session.get('client_name') != None:
@@ -69,12 +131,7 @@ def bot_talk():
 
 
     '''text response'''
-    #broken up responses
-    # for i,v in enumerate(text_portion.rsplit("\n")):
-    # 	print(i)
-    # 	resp.message(msg=text_portion.rsplit("\n")[i])
-
-    #fat responses
+   
     resp.message(msg=text_portion)
 
     '''image response'''
